dcd9e09421132ba36e7ef6053eee4174de7c4648
Keldiyorov Bekzod <bekhzodkeldiyorov@gmail.com>
2025-10-28 14:23:25 +0500

    add

---
+++ backend/RenessansAPI.DataAccess/AppDBContexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RenessansAPI.Domain.Entities.Auth;
+using RenessansAPI.Domain.Entities.Users;
+
+namespace RenessansAPI.DataAccess.AppDBContexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    //Auth
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<Role> Roles { get; set; }
+    public virtual DbSet<Permission> Permissions { get; set; }
+    public virtual DbSet<Token> Tokens { get; set; }
+}
